1364c2a34c0a73fb4d7cdb7c3f30146e2ecc7950
JeyaAnanthi03 <sokkalingamthangam@gmail.com>
2025-10-03 16:06:53 +0530

    Project completed

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ import SignUpPage from "./routes/sign-up";
 import SigninPage from "./routes/sign-in";
 import ProtectedRoutes from "./layouts/protected-routes";
 import MainLayout from "./layouts/main-layout";
+import Generate from "./components/generate";
+import Dashboard from "./routes/dashboard";
+import CreateEditPage from "./components/create-edit-page";
+import MockLoadPage from "./routes/mock-load-page";
+import { MockInterviewPage } from "./routes/mock-interview-page";
+import { Feedback } from "./routes/feedback";
+
 const App = () => {
   return (
     <Router>
@@ -26,7 +33,19 @@ const App = () => {
               <MainLayout />
             </ProtectedRoutes>
           }
-        ></Route>
+        >
+          {/* Add all protected routes */}
+          <Route element={<Generate />} path="/generate">
+            <Route index element={<Dashboard />} />
+            <Route path=":interviewId" element={<CreateEditPage />} />
+            <Route path="interview/:interviewId" element={<MockLoadPage />} />
+            <Route
+              path="interview/:interviewId/start"
+              element={<MockInterviewPage />}
+            />
+            <Route path="feedback/:interviewId" element={<Feedback />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
